6a2de708a0deb1db98ef326a5370e3ec068afc71
oguzdeniz <ahmetsert@gmail.com>
2023-01-30 22:58:18 +0300

    mouseover effect done

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ console.log('hello world!')
 
 const container = document.getElementById('container')
 
+function changeBgColor(el) {
+  el.style.background = 'blue'
+}
+
 for (let i = 0; i < 256; i++) {
   const square = document.createElement('div')
   const list = square.classList
   list.add('square')
   container.appendChild(square)
+  square.addEventListener('mouseover', () => changeBgColor(square))
 }
